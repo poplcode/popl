0b50af3520ab9167632c7e55e5816ed3a7ca0e07
poplcode <code@popl.ml>
2020-03-09 16:56:05 +0900

    response_redirect에서 기본값을 '/' 로 설정

--- popl/lib/response.php
+++ popl/lib/response.php
@@ -1,5 +1,5 @@
 <?php
-function popl_response_redirect($url, $http_status_code='301'){
+function popl_response_redirect($url='/', $http_status_code='301'){
     header("Location: $url", true, $http_status_code);
     exit();
 }
