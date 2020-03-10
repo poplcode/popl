1b7795cc4ae175d2b9ca03eac3d71c20e9397369
poplcode <code@popl.ml>
2020-03-11 00:56:02 +0900

    popl_response_redirect_flash 추가

--- popl/lib/response.php
+++ popl/lib/response.php
@@ -3,3 +3,8 @@ function popl_response_redirect($url='/', $http_status_code='301'){
     header("Location: $url", true, $http_status_code);
     exit();
 }
+
+function popl_response_redirect_flash($url='/', $message='', $http_status_code='301'){
+    popl_flash_set($message);
+    popl_response_redirect($url, $http_status_code);
+}
