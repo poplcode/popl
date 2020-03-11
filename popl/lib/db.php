5a7341449a1abfbd7ddc32a64768da1e5c975f20
poplcode <code@popl.ml>
2020-03-11 16:18:33 +0900

    popl_db 함수 추가

--- popl/lib/db.php
+++ popl/lib/db.php
@@ -49,9 +49,9 @@ function popl_db_fetch_all($query, $param){
     return $result;
 }
 
-function popl_db_make_terms($kvParam=[]){
+function popl_db_make_terms($where_terms=[]){
     $terms = array();
-    foreach ($kvParam as $key=>$value){
+    foreach ($where_terms as $key=>$value){
         array_push($terms, "$key = :$key");
     }
 
@@ -92,12 +92,12 @@ function set_upsert_date_column($row){
     return $row;
 }
 
-function popl_db_select($table_name, $kvParam=[], $orderby='', $limit=null){
-    $terms = popl_db_make_terms($kvParam);
+function popl_db_select($table_name, $where_terms=[], $orderby='', $limit=null){
+    $terms = popl_db_make_terms($where_terms);
     $orderby = popl_db_make_orderby($orderby);
     $limit = popl_db_make_limit($limit);
     $query = "select * from $table_name $terms $orderby $limit";
-    $result =  popl_db_fetch_all($query, $kvParam);    
+    $result =  popl_db_fetch_all($query, $where_terms);    
     foreach ($result as $idx => $row){
         $row = set_upsert_date_column($row);
         $result[$idx] = $row;
@@ -105,8 +105,8 @@ function popl_db_select($table_name, $kvParam=[], $orderby='', $limit=null){
     return $result;
 }
 
-function popl_db_select_first($table_name, $kvParam=[], $orderby=''){
-    $result = popl_db_select($table_name, $kvParam, $orderby, 1);
+function popl_db_select_first($table_name, $where_terms=[], $orderby=''){
+    $result = popl_db_select($table_name, $where_terms, $orderby, 1);
     if (count($result) == 1){
         $result = $result[0];        
         return $result;
@@ -119,14 +119,14 @@ function popl_db_select_first_by_id($table_name, $id){
     return popl_db_select_first($table_name, ['id'=>$id]);
 }
 
-function popl_db_select_exist($table_name, $kvParam=[]){
-    $result = popl_db_select_first($table_name, $kvParam);    
+function popl_db_select_exist($table_name, $where_terms=[]){
+    $result = popl_db_select_first($table_name, $where_terms);    
     return $result !== null;
 }
 
-function popl_db_select_paging($table_name, $page_no, $kvParam=[], $orderby='insert_date desc', $count_per_page=10){
+function popl_db_select_paging($table_name, $page_no, $where_terms=[], $orderby='insert_date desc', $count_per_page=10){
     $offset = ($page_no -1) * $count_per_page;
-    return popl_db_select($table_name, $kvParam, $orderby, "$offset,$count_per_page");
+    return popl_db_select($table_name, $where_terms, $orderby, "$offset,$count_per_page");
 }
 
 function popl_db_insert($table_name, $kvparam=[]){    
@@ -163,4 +163,62 @@ function popl_db_insert_standard($table_name, $kvparam){
     $kvparam = popl_db_set_default_values($kvparam, $default_key_vals);
 
     return popl_db_insert($table_name, $kvparam);    
+}
+
+function popl_db_update($table_name, $kvparam, $where_terms){
+    // 연관배열에서 키만 가져오기.
+    $columns = array_keys($kvparam);
+
+    // 배열의 각 아이템에 항목 적용시키기.
+    $placeholders = array_map(
+        function($key){
+            return "$key = :$key";
+        },
+        $columns);
+
+    $strPlaceHolders = implode(",",$placeholders);
+    
+    $terms = popl_db_make_terms($where_terms);
+    $total_kv = array_merge($kvparam, $where_terms);
+    
+    $query = "update $table_name set $strPlaceHolders $terms";
+    $result = $this->execute($query,$total_kv);
+    return $result;
+}
+
+function popl_db_update_standard($table_name, $kvparam, $where_terms){
+    $default_key_vals = [        
+        "update_date" => date("Y-m-d H:i:s")
+    ];
+
+    $kvparam = popl_db_set_default_values($kvparam, $default_key_vals);
+
+    return popl_db_update($table_name, $kvparam, $where_terms);    
+}
+
+function popl_db_upsert($table_name, $kvparam, $where_terms){
+    if (popl_db_select_exist($table_name, $where_terms)){
+        popl_db_update($table_name, $kvparam, $where_terms);
+    }else{
+        popl_db_insert($table_name, $kvparam);
+    }
+}
+
+function popl_db_upsert_standard($table_name, $kvparam, $where_terms){
+    if (popl_db_select_exist($table_name, $where_terms)){
+        popl_db_update_standard($table_name, $kvparam, $where_terms);
+    }else{
+        popl_db_insert_standard($table_name, $kvparam);
+    }
+}
+
+function popl_db_delete($table_name, $where_terms){
+    $terms = popl_db_make_terms($where_terms);    
+    $query = "delete from $table_name terms";
+    return popl_db_execute($query, $where_terms);
+}
+
+function popl_db_delete_by_id($table_name, $id){
+    $where_terms = ['id'=>$id];
+    return popl_db_delete($table_name, $where_terms);
 }
