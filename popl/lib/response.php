9a324bd5aba46873cd0acbe175afe8f3d58f042c
poplcode <code@popl.ml>
2020-03-16 01:10:47 +0900

    popl_response_redirect_post 추가 popl_response_redirect_post_flash 추가

--- popl/lib/response.php
+++ popl/lib/response.php
@@ -8,3 +8,28 @@ function popl_response_redirect_flash($url='/', $message='', $http_status_code='
     popl_flash_set($message);
     popl_response_redirect($url, $http_status_code);
 }
+
+function popl_response_redirect_post($url='/', $params=[]){
+    $input_tags = '';
+    foreach($params as $key=>$val){
+        $input_tags .= "<input type='hidden' name='$key' value='$val' />" . PHP_EOL;
+    }
+    $html = <<<CDATA
+    <html><head></head><body>
+    <form method='post' id='frm' name='frm' action='$url'>    
+    $input_tags
+    </form>
+    <script type="text/javascript">
+        document.getElementById('frm').submit();
+    </script>
+    </body></html>
+CDATA;
+    echo $html;
+    exit();
+
+}
+
+function popl_response_redirect_post_flash($url='/', $params=[], $message=''){
+    popl_flash_set($message);
+    popl_response_redirect_post($url, $params);
+}
