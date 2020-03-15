b83e855663debff6d674944ab0c8ad46fd8aa949
poplcode <code@popl.ml>
2020-03-16 01:08:00 +0900

    popl_array_map popl_array_join popl_array_map_join 추가

--- popl/lib/arr.php
+++ popl/lib/arr.php
@@ -2,3 +2,15 @@
 function popl_array_has_key($arr, $key){
     return array_key_exists($key, $arr);
 }
+
+function popl_array_map($arr, $func){
+    return array_map($func, $arr);
+}
+
+function popl_array_join($arr, $glue=''){
+    return implode($glue, $arr);
+}
+
+function popl_array_map_join($arr, $func, $glue=''){
+    return popl_array_join(popl_array_map($arr, $func), $glue);
+}
