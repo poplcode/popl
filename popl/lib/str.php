8365b12581e6312af6863eb87565b825ecf96f7f
poplcode <code@popl.ml>
2020-03-16 01:12:18 +0900

    popl_str_splitlines 추가 popl_str_wrap_each_lines 추가

--- popl/lib/str.php
+++ popl/lib/str.php
@@ -20,6 +20,18 @@ function popl_str_split($input, $seperater){
     return explode($seperater, $input);
 }
 
+function popl_str_splitlines($input){
+    return popl_str_split($input, PHP_EOL);
+}
+
 function popl_str_replace($input, $old_str, $new_str){
     return str_replace($old_str, $new_str, $input);
+}
+
+function popl_str_wrap_each_lines($input, $prefix_item='',$suffix_item=''){
+    return popl_array_map_join(
+        popl_str_splitlines($input),
+        function($item) use ($prefix_item, $suffix_item){return "$prefix_item$item$suffix_item";},
+        PHP_EOL
+    );
 }
