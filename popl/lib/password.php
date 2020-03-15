ed8a9d801e0770ba460b4abfb1bd49fd744648d0
poplcode <code@popl.ml>
2020-03-16 01:10:04 +0900

    popl_password_match 추가

--- popl/lib/password.php
+++ popl/lib/password.php
@@ -3,3 +3,7 @@
 function popl_password_encrypt($input){
     return password_hash($input, PASSWORD_BCRYPT);
 }
+
+function popl_password_match($plain_input, $hash){
+    return password_verify($plain_input, $hash);
+}
